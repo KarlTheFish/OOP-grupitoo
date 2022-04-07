a1d2e83c1514475a958a995773d86fe66bf87e2a
Taavi Pahk <taavipahk43@gmail.com>
2022-04-07 21:17:26 +0300

    Grammatika ja natuke JS veel

--- src/main/resources/static/scripts/sub_script.js
+++ src/main/resources/static/scripts/sub_script.js
@@ -1,4 +1,15 @@
 const submitButton = document.querySelector("#submit");
+const subInput = document.querySelector("#sub");
+const letterAmount = document.querySelector("#letterAmount");
+
+subInput.addEventListener("input", function() {
+    letterAmount.textContent = subInput.value.length + " tähte";
+    if(subInput.value.length > 26) {
+        letterAmount.style.color = "red";
+    } else {
+        letterAmount.style.color = "initial";
+    }
+}) ;
 
 submitButton.addEventListener("click", function() {
     const text = document.querySelector("#text").value;
