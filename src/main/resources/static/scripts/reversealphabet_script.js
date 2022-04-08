457afbab29f3ef5fb94292cdf0a5988512713f06
Taavi Pahk <taavipahk43@gmail.com>
2022-04-08 12:26:01 +0300

    Frontend valmis?

---
+++ src/main/resources/static/scripts/reversealphabet_script.js
@@ -0,0 +1,17 @@
+const submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click", function() {
+    const text = document.querySelector("#text").value;
+    if(!text) {
+        document.getElementById("result").textContent = "Miski on puudu.";
+        return;
+    }
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("result").textContent = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", `ReverseAlphabetCipher/${text}`, true);
+    xhttp.send();
+});
