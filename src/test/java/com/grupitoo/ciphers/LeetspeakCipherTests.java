8b8c951187b5580e5a6a0f6ee3cfc4d9bf87965a
Karl <karlthefish@github.com>
2022-04-08 11:16:18 +0300

    Lisatud testid

--- src/test/java/com/grupitoo/ciphers/LeetspeakCipherTests.java
+++ src/test/java/com/grupitoo/ciphers/LeetspeakCipherTests.java
@@ -1,12 +1,24 @@
 package com.grupitoo.ciphers;
 
-//see on minu klasside testimiseks, ära puutu pls ma kustutan hiljem ära -Karl
-
 import com.grupitoo.ciphers.ciphers.LeetcodeCipher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class LeetspeakCipherTests {
-    public static void main(String[] args){
-        LeetcodeCipher hi = new LeetcodeCipher("hi");
-        System.out.println(hi.makeLeet("German techno hardstyle"));
+@SpringBootTest
+class LeetSpeakCipherTests {
+    LeetcodeCipher testCipher = new LeetcodeCipher("");
+
+
+    @Test
+    void Tekst_Iga_Tahte_1() {
+        this.testCipher = new LeetcodeCipher(testCipher.makeLeet("My Dog is Lazy"));
+        Assertions.assertEquals("|\\/| y |) 0 &  1 5  [_ 4 zy", testCipher.makeLeet("My Dog is Lazy"));
+    }
+
+    @Test
+    void Tekst_Korduvad_Tahed() {
+        this.testCipher = new LeetcodeCipher(testCipher.makeLeet("I like Ice cream"));
+        Assertions.assertEquals("1  [_ 1 |< 3  1 < 3  < |? 3 4 |\\/| ", testCipher.makeLeet("I like Ice cream"));
     }
 }
--- src/test/java/com/grupitoo/ciphers/ReverseAlphabetCipherTests.java
+++ src/test/java/com/grupitoo/ciphers/ReverseAlphabetCipherTests.java
@@ -1,11 +1,25 @@
 package com.grupitoo.ciphers;
 
+import com.grupitoo.ciphers.ciphers.LeetcodeCipher;
 import com.grupitoo.ciphers.ciphers.ReverseAlphabetCipher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class ReverseAlphabetCipherTests {
+@SpringBootTest
+class ReverseAlphabetCipherTests {
+    ReverseAlphabetCipher testCipher = new ReverseAlphabetCipher("");
 
-    public static void main(String[] args){
-        ReverseAlphabetCipher test = new ReverseAlphabetCipher("hi there");
-        System.out.println(test.ReverseAlphabet("r orpv rxv xivzn"));
+
+    @Test
+    void Tekst_Iga_Tahte_1(){
+        this.testCipher = new ReverseAlphabetCipher(testCipher.ReverseAlphabet("My Dog is Lazy"));
+        Assertions.assertEquals("nb wlt rh ozab", testCipher.ReverseAlphabet("My Dog is Lazy"));
+    }
+
+    @Test
+    void Tekst_Korduvad_Tahed() {
+        this.testCipher = new ReverseAlphabetCipher(testCipher.ReverseAlphabet("I like Ice Cream"));
+        Assertions.assertEquals("r orpv rxv xivzn", testCipher.ReverseAlphabet("I like Ice Cream"));
     }
 }
